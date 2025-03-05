6ac68ad78271754660dd5e421443961273431f0f
Faraja Athanas <80798761+Farajaat21@users.noreply.github.com>
2025-03-05 21:14:49 +0000

    new detail

--- app.js
+++ app.js
@@ -1,19 +1,33 @@
 let gameState = ['', '', '', '', '', '', '', '', ''];
 let currentPlayer = 'X';
 
+const playerImages = {
+    'X': 'img/icons8-squid-game-circle-guard-96.png',
+    'O': 'img/icons8-squid-game-triangle-guard-96.png'
+}
+
 function makeMove(curIndex) {
     if (gameState[curIndex] === '') {
+
         gameState[curIndex] = currentPlayer;
-        document.getElementById(`cell-${curIndex}`).innerText = currentPlayer;
 
         
+
+        document.getElementById(`cell-${curIndex}`).innerHTML = `<img src="${playerImages[currentPlayer]}" alt="${currentPlayer}" style="width: 80px; height: 80px;">`;
+
         if (checkWinner()) {
             document.getElementById('message').innerText = `${currentPlayer} wins!`;
+            setTimeout(resetGame, 2000);
             return;
+            
         } else if (gameState.every(cell => cell !== '')) {
             document.getElementById('message').innerText = `It's a draw!`;
+            setTimeout(resetGame, 2000);
             return;
         }
+        
+
+        
         currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
     }
 }
@@ -28,4 +42,14 @@ function checkWinner() {
     return winningCombinations.some(combination => {
         return combination.every(index => gameState[index] === currentPlayer);
     });
+}
+function resetGame(){
+   
+    gameState = ['', '', '', '', '', '', '', '', ''];
+
+    for(let i = 0; i < gameState.length; i++){
+        document.getElementById(`cell-${i}`).innerHTML = "";
+    }
+    currentPlayer = "X"
+    document.getElementById("message").innerText = "";
 }
